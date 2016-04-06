c89ca424b07a6d533dc18d273f8e1e5bbdc099bf
nabil <nabil.nachit@old-banana.fr>
2016-04-06 10:17:48 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# zimlet-jitsi-meet
+# Jitsi-meet-Zimlet
+
+This zimlet allows you to add a Jitsi Meet URL your message body (email and appointement)
+
+This software is released under the terms of the GPL v2. See the included LICENCE.TXT for details.
+
+
---
+++ fr_beezim_jitsi/fr_beezim_jitsi.js
@@ -0,0 +1,67 @@
+function fr_beezim_jitsi() {}
+
+fr_beezim_jitsi.prototype = new ZmZimletBase();
+fr_beezim_jitsi.prototype.constructor = fr_beezim_jitsi;
+
+function Jitsi() {}
+
+Jitsi = fr_beezim_jitsi;
+
+Jitsi.prototype.init = function() {
+    // zimlet init
+    // editor HTML : "text/html", plain : "text/plain"
+};
+
+Jitsi.prototype.initializeToolbar = function(app, toolbar, controller, view) {
+    // Add a jitsi button to the toolbar if current view is COMPOSE or APPT
+    if (view.indexOf("COMPOSE") >= 0 || view.indexOf("APPT") >=0 ) {
+        var buttonIndex = 9;
+        var buttonArgs = {
+            text: "Jitsi meet", //TODO: String should be in the properties file
+            tooltip: "Create a Jitsi meet URL", //TODO: String should be in the properties file
+            index: buttonIndex,
+            image: "zimbraicon",
+            enabled: true
+        };
+        var button = toolbar.createOp("JITSI", buttonArgs);
+        button.addSelectionListener(new AjxListener(this, this._createJitsiURL));
+    }
+};
+
+Jitsi.prototype._createJitsiURL = function() {
+    var string = this._getRandomValue();
+    var URL = "https://meet.jit.si/"; //TODO: String should be in the properties file
+    var jitsiURL = URL + string;
+    this._insertURL(jitsiURL);
+};
+
+Jitsi.prototype._getRandomValue = function() {
+    return (Math.random() + 1).toString(36).slice(2);
+};
+
+Jitsi.prototype._insertURL =
+function(jitsiURL) {
+    var cc = appCtxt.getCurrentView();
+    var currentEditorMode = cc.getComposeMode();
+    if (currentEditorMode == "text/html") {
+        var URL = "<a href=" + jitsiURL + " data-mce-href=" + jitsiURL + ">Click Here to join the meeting</a>"; //TODO: String should be in the properties file
+    }
+    else {
+        var URL = jitsiURL;
+    }
+	var composeView = appCtxt.getCurrentView();
+	var currentBodyContent = currentBodyContent = appCtxt.getCurrentView().getHtmlEditor().getContent();
+	var params = {URL: URL,  currentBodyContent:currentBodyContent, composeView:composeView};
+	this._doInsertURL(params, composeView);
+};
+
+Jitsi.prototype._doInsertURL =
+function(params, composeView) {
+    var separator = "\r\n";
+    if (composeView.__internalId.indexOf("APPT") >= 0) {
+        composeView.getHtmlEditor().setContent([params.currentBodyContent, separator, params.URL].join(""));
+    }
+    else {
+        composeView._htmlEditor.setContent([params.currentBodyContent, separator, params.URL].join(""));
+    }
+};
